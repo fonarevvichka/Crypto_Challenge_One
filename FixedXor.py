1c912d838ce463967bbe36fb2d967612d80fd678
Vichka Fonarev <fonarev.vichka@gmail.com>
2016-10-17 22:35:37 -0400

    challenge 2 done

--- FixedXor.py
+++ FixedXor.py
@@ -1,6 +1,7 @@
 import conversions
-from operator import xor
+from binascii import hexlify
 
 def fixedXorCompare(s):
-    return conversions.deHex(s) ^ conversions.deHex("686974207468652062756c6c277320657965")
+    return hex(int(s, 16) ^ int("686974207468652062756c6c277320657965", 16))[2:]
+
 print(fixedXorCompare("1c0111001f010100061a024b53535009181c"))
--- conversions.py
+++ conversions.py
@@ -4,6 +4,4 @@ import base64
 def hexToBase64(s):
     rawBytes = unhexlify(s)
     return base64.b64encode(rawBytes)
-def deHex(s):
-    return unhexlify(s)
 
